8c6c63a761fa73d5175160f302a51840dd4567de
Elías Gámez García <99614139+egamezg2122@users.noreply.github.com>
2022-03-22 12:18:01 +0100

    First comments

--- EldenArk/src/eldenark/EldenArk.java
+++ EldenArk/src/eldenark/EldenArk.java
@@ -1,9 +1,19 @@
+/* EldenArk
+ * Version 1.0
+ * Elías Gámez
+ * Marcel Homar
+ * Albert Salazar
+ * 
+ */
+
 
 package eldenark;
 
 public class EldenArk {
 
     public static void main(String[] args) {
+        
+        // To do MAIN code
         
     }
     
