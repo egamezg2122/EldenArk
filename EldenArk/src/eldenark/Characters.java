dfbd7601873031b4dad1a71f217649d89529dfac
leoFerraz19 <99325374+leoFerraz19@users.noreply.github.com>
2022-03-25 12:17:55 +0100

    fight cositas

--- EldenArk/src/eldenark/Characters.java
+++ EldenArk/src/eldenark/Characters.java
@@ -2,188 +2,238 @@ package eldenark;
 
 public class Characters {
 
-    // Main atributes
-    private int level;
+	// Main atributes
+	private int level;
 
-    private int xp; // Experience Points
+	private int xp; // Experience Points
 
-    private int damage;
+	private int damage;
 
-    private int defense;
+	private int defense;
 
-    private int hp; // Health Points
+	private int hp; // Health Points
 
-    private int maxHP;
+	private int maxHP;
 
-    private int mp; // Mana Points
+	private int mp; // Mana Points
 
-    private int maxMP;
+	private int maxMP;
 
-    private int specialDamage;
+	private int specialDamage;
 
-    private int x = 5; // Position in the map
-	
+	private int x = 5; // Position in the map
+
 	private int y = 5; // Position in the map
 
-    // Constructors
-    public Characters(int damage, int defense, int hp, int maxHP, int mp, int maxMP, int specialDamage) {
+	// Constructors
+	public Characters(int damage, int defense, int hp, int maxHP, int mp, int maxMP, int specialDamage) {
 
-        this.level = 1; // Always start at level 1
+		this.level = 1; // Always start at level 1
 
-        this.xp = 0; // Experience always start at 0
+		this.xp = 0; // Experience always start at 0
 
-        this.damage = damage;
+		this.damage = damage;
 
-        this.defense = defense;
+		this.defense = defense;
 
-        this.hp = hp;
+		this.hp = hp;
 
-        this.maxHP = maxHP;
+		this.maxHP = maxHP;
 
-        this.mp = mp;
+		this.mp = mp;
 
-        this.maxMP = maxMP;
+		this.maxMP = maxMP;
 
-        this.specialDamage = specialDamage;
+		this.specialDamage = specialDamage;
 
-    }
+	}
 
-    public Characters() {
-    }
+	public Characters() {
+	}
 
-    //METHODS
-    //ESTARIA BIEN QUE LVLUP FUESE ABSTRACT
-    public void levelUp() {
-        level++;
-        damage = damage * 2;
-        defense = defense * 2;
-        specialDamage = specialDamage * 2;
-        hp = (maxHP * 2) - (maxHP - hp);
-        maxHP = maxHP * 2;
-        mp = (maxMP * 2) - (maxMP - mp);
-        maxMP = maxMP * 2;
-        if (level % 5 == 0) {
-            //get new hability
-            System.out.println("NEW ABILITY MARICONNNNN");
-        }
-    }
+	//METHODS
+	//ESTARIA BIEN QUE LVLUP FUESE ABSTRACT
+	public void levelUp() {
+		level++;
+		damage = damage * 2;
+		defense = defense * 2;
+		specialDamage = specialDamage * 2;
+		hp = (maxHP * 2) - (maxHP - hp);
+		maxHP = maxHP * 2;
+		mp = (maxMP * 2) - (maxMP - mp);
+		maxMP = maxMP * 2;
+		if (level % 5 == 0) {
+			//get new hability
+			System.out.println("NEW ABILITY MARICONNNNN");
+		}
+	}
 
-    //Funtion for showing the stats of the character
-    public void showStats() {
-        /*
+	//Funtion for showing the stats of the character
+	public void showStats() {
+		/*
 		The first String has a maximum length of 15 and has 25 blank spaces. The text is left justified.
 		Then, right jutifed we have the value of each atribute
-         */
-        System.out.println(String.format("%-25.15s %10d", "LEVEL", level));
-        System.out.println(String.format("%-25.15s %10d", "Damage", damage));
-        System.out.println(String.format("%-25.15s %10d", "Defense", defense));
-        System.out.println(String.format("%-25.15s %10d", "Special Damage", specialDamage));
-        System.out.println(String.format("%-25.15s %10s", "HP", hp + "/" + maxHP));
-        System.out.println(String.format("%-25.15s %10s", "MP", mp + "/" + maxMP));
-    }
+		 */
+		System.out.println(String.format("%-25.15s %10d", "LEVEL", level));
+		System.out.println(String.format("%-25.15s %10d", "Damage", damage));
+		System.out.println(String.format("%-25.15s %10d", "Defense", defense));
+		System.out.println(String.format("%-25.15s %10d", "Special Damage", specialDamage));
+		System.out.println(String.format("%-25.15s %10s", "HP", hp + "/" + maxHP));
+		System.out.println(String.format("%-25.15s %10s", "MP", mp + "/" + maxMP));
+	}
 
-    public void move() {
-        x++;
-    }
+	public void move() {
+		x++;
+	}
+	
+	public Characters createEnemy(){
+		Characters enemy = new Characters();
+		
+		return enemy;
+	}
+	
+	//ES NECESITADO EL CAMBIO A ESTA SECCION DEL CODIGO
+	public int fightMenu() {
+		System.out.println("\n1-Basic Attack" + "\n2-Defend" + "\n3-Special Abilities" + "\n4-Objects");
+		return Teclat.llegirInt();
+	}
 
-    //ES NECESITADO EL CAMBIO A ESTA SECCION DEL CODIGO
-    public int fightMenu() {
-        System.out.println("1-Basic Attack" + "\n2-Defend" + "\n3-Special Abilities" + "\n4-Objects");
-        return 1;
-
-    }
-
-    public void fight() {
-        
-    }
-
-    public void menu() {
-        int op;
-        System.out.println("\nMENU\n");
-        System.out.println("What do you want to do?");
-        //Print the menu
-        op = Teclat.llegirInt();
-        switch (op) {
-            case 0:
-            //EXIT THE GAME
+	public void basicAttack(Characters enemy){
+		enemy.setHp(hp-damage);
+	}
+	
+	public void defend(){
+		
+	}
+	
+	public void specialAbilities(Characters enemy){
+		enemy.setHp(hp-specialDamage);
+	}
+	
+	public void objects(){
+		
+	}
+	
+	public void fight(Characters enemy) {
+		int option;
+		do{
+			System.out.println("How are you gonna fight?");
+			option = fightMenu();
+			switch(option){
+			case 1:
+				//BASIC ATTACK
+				basicAttack(enemy);
 				break;
-            case 1:
-                //MOVE
-                move();
+			case 2:
+				//DEFEND
+				defend();
 				break;
-            case 2:
-            //FIGHT
-                fight();
+			case 3:
+				//SPECIAL ABILITIES
+				specialAbilities(enemy);				
 				break;
-            case 3:
-            //LOOT
+			case 4:
+				//OBJECTS
+				objects();
 				break;
-            case 4:
-            //INVENTORY
+			default:
+				System.err.println("\nInvalid option");
+		}
+		}while(enemy.getHp()>0);
+		
+		
+		
+		
+	}
+
+	public void menu() {
+		int op;
+		System.out.println("\nMENU\n");
+		System.out.println("What do you want to do?");
+		System.out.println("\n\t0- Exit the game \n\t1- Move the character \n\t2- Fight \n\t3- Loot \n\t4- Show inventory \n\t5- Check Stats");
+		//Print the menu
+		op = Teclat.llegirInt();
+		switch (op) {
+			case 0:
+				//EXIT THE GAME
 				break;
-            case 5:
-                //CHECK STATS
-                showStats();
+			case 1:
+				//MOVE
+				move();
 				break;
-            default:
-                System.err.println("Error,invalid option." + " \nTry again");
-        }
-    }
+			case 2:
+				//FIGHT
+				fight(createEnemy());
+				break;
+			case 3:
+				//LOOT
+				break;
+			case 4:
+				//INVENTORY
+				break;
+			case 5:
+				//CHECK STATS
+				showStats();
+				break;
+			default:
+				System.err.println("Error,invalid option." + " \nTry again");
+		}
+	}
 
-    // Getters
-    public int getLevel() {
+	// Getters
+	public int getLevel() {
 
-        return level;
+		return level;
 
-    }
+	}
 
-    public int getXp() {
+	public int getXp() {
 
-        return xp;
+		return xp;
 
-    }
+	}
 
-    public int getDamage() {
+	public int getDamage() {
 
-        return damage;
+		return damage;
 
-    }
+	}
 
-    public int getDefense() {
+	public int getDefense() {
 
-        return defense;
+		return defense;
 
-    }
+	}
 
-    public int getHp() {
+	public int getHp() {
 
-        return hp;
+		return hp;
 
-    }
+	}
 
-    public int getMaxHP() {
+	public int getMaxHP() {
 
-        return maxHP;
+		return maxHP;
 
-    }
+	}
 
-    public int getMp() {
+	public int getMp() {
 
-        return mp;
+		return mp;
 
-    }
+	}
 
-    public int getMaxMP() {
+	public int getMaxMP() {
 
-        return maxMP;
+		return maxMP;
 
-    }
+	}
 
-    public int getSpecialDamage() {
+	public int getSpecialDamage() {
 
-        return specialDamage;
+		return specialDamage;
 
-    }
+	}
 
 	public int getX() {
 		return x;
@@ -192,62 +242,60 @@ public class Characters {
 	public int getY() {
 		return y;
 	}
-	
-	
 
-    // Setters
-    public void setLevel(int level) {
+	// Setters
+	public void setLevel(int level) {
 
-        this.level = level;
+		this.level = level;
 
-    }
+	}
 
-    public void setXp(int xp) {
+	public void setXp(int xp) {
 
-        this.xp = xp;
+		this.xp = xp;
 
-    }
+	}
 
-    public void setDamage(int damage) {
+	public void setDamage(int damage) {
 
-        this.damage = damage;
+		this.damage = damage;
 
-    }
+	}
 
-    public void setDefense(int defense) {
+	public void setDefense(int defense) {
 
-        this.defense = defense;
+		this.defense = defense;
 
-    }
+	}
 
-    public void setHp(int hp) {
+	public void setHp(int hp) {
 
-        this.hp = hp;
+		this.hp = hp;
 
-    }
+	}
 
-    public void setMaxHP(int maxHP) {
+	public void setMaxHP(int maxHP) {
 
-        this.maxHP = maxHP;
+		this.maxHP = maxHP;
 
-    }
+	}
 
-    public void setMp(int mp) {
+	public void setMp(int mp) {
 
-        this.mp = mp;
+		this.mp = mp;
 
-    }
+	}
 
-    public void setMaxMP(int maxMP) {
+	public void setMaxMP(int maxMP) {
 
-        this.maxMP = maxMP;
+		this.maxMP = maxMP;
 
-    }
+	}
 
-    public void setSpecialDamage(int specialDamage) {
+	public void setSpecialDamage(int specialDamage) {
 
-        this.specialDamage = specialDamage;
+		this.specialDamage = specialDamage;
 
-    }
+	}
 
 }
