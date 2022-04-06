55ca5c83ab22d4c811f1ae7bc946bb8a6a64e423
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-04-06 10:37:43 +0200

    Combat test

--- EldenArk/src/eldenark/Combat.java
+++ EldenArk/src/eldenark/Combat.java
@@ -5,9 +5,16 @@
  */
 package eldenark;
 
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -16,70 +23,71 @@ import javax.swing.JPanel;
  */
 public class Combat extends JFrame {
 
+	JFrame f;
 	JPanel prueba;
+	Character mainCharacter;
+	Character enemy;
+	JLabel enemyImage;
 
 	Combat(Character mainCharacter, Character enemy) {
-
+		this.mainCharacter = mainCharacter;
+		this.enemy = enemy;
+		
 		setFrame();
 		
 
 	}
+	
+	
 
 	public void setFrame() {
-		JFrame f = new JFrame("Button Example");
-		JButton b = new JButton("Atack");
-		b.setBounds(10, 300, 100, 50);
-		JButton c = new JButton("Defence");
-		c.setBounds(120, 300, 100, 50);
-		JButton d = new JButton("Abilities");
-		d.setBounds(230, 300, 100, 50);
-		f.add(b);
-		f.add(c);
-		f.add(d);
-		f.setSize(400, 400);
+		
+		f = new JFrame("Combat");
+		ImageIcon icon = new ImageIcon("KAYO_icon.png");
+		JButton attack = new JButton("Basic Attack");
+		attack.setBounds(10, 300, 100, 50);
+		JButton defence = new JButton("Defence");
+		defence.setBounds(120, 300, 100, 50);
+		JButton abilities = new JButton("Abilities");
+		abilities.setBounds(230, 300, 100, 50);
+		JButton objects = new JButton("Objects");
+		objects.setBounds(340, 300, 100, 50);
+		JLabel nombre = new JLabel(icon);
+		f.add(nombre);
+		f.pack();
+		f.add(attack);
+		f.add(defence);
+		f.add(abilities);
+		f.add(objects);
+		
+		f.setLayout(new LayoutManager());
+		
+        f.setContentPane(new JLabel(new ImageIcon("fondo.jfif")));
+        f.setLayout(new FlowLayout());
+        // Just for refresh :) Not optional!
+        
+		
+		f.setSize(599, 399);
+		f.setSize(600, 400);
 		f.setLayout(null);
 		f.setVisible(true);
 
-		b.addActionListener((ActionEvent e) -> {
-			System.out.println("a");
-			
+		attack.addActionListener((ActionEvent e) -> {
+			System.out.println("Basic Attack");
+			nombre.setLocation(nombre.getX() + 20, nombre.getY());
 		});
-		c.addActionListener((ActionEvent e) -> {
-			System.out.println("b");
-			
+		defence.addActionListener((ActionEvent e) -> {
+			System.out.println("Caldo de pollo");
 		});
-		d.addActionListener((ActionEvent e) -> {
-			f.setVisible(false);
+		abilities.addActionListener((ActionEvent e) -> {
 			changeFrame();
-			
 		});
 	}
 
 	public void changeFrame() {
 
-		JFrame f = new JFrame("Maricon");
-		JButton b = new JButton("a");
-		b.setBounds(10, 300, 100, 50);
-		JButton c = new JButton("b");
-		c.setBounds(120, 300, 100, 50);
-		JButton d = new JButton("c");
-		d.setBounds(230, 300, 100, 50);
-		f.add(b);
-		f.add(c);
-		f.add(d);
-		f.setSize(400, 400);
-		f.setLayout(null);
-		f.setVisible(true);
-
-		b.addActionListener((ActionEvent e) -> {
-			System.out.println("Bobolon");
-		});
-		c.addActionListener((ActionEvent e) -> {
-			System.out.println("Caldo de pollo");
-		});
-		d.addActionListener((ActionEvent e) -> {
-			changeFrame();
-		});
 	}
 
 }
+
+
