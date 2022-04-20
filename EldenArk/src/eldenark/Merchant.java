1f767ff194068485e472db10c12e2c0e6d8e709a
AlbertPolitecnis <99322526+AlbertPolitecnis@users.noreply.github.com>
2022-04-20 12:36:15 +0200

    Rogelio things
    ROGELIO

--- EldenArk/src/eldenark/Merchant.java
+++ EldenArk/src/eldenark/Merchant.java
@@ -13,213 +13,291 @@ import java.util.Random;
  */
 public class Merchant {
 
-	Random rn = new Random();
-
-	private String name;
-	private int y;
-	private int x;
-	private int floor;
-	private Character mainCharacter;
-	private Object sellingObject1;
-	private Object sellingObject2;
-	private Equip sellingEquip;
-	//private Weapon sellingWeapon1;
-	private int priceSmallObject = 20;
-	private int priceBigObject = 50;
-	private int priceEquip;
-	//private int priceWeapon = 500;
-	private int quantitySmallObject = 5;
-	private int quantityBigObject = 2;
-	private int quantityEquip = 1;
-	//private int quantityWeapon;
-	private int typeObject1;
-	private int typeObject2;
+    Random rn = new Random();
+
+    private String name;
+    private int y;
+    private int x;
+    private int floor;
+    private Character mainCharacter;
+    private Object sellingObject1;
+    private Object sellingObject2;
+    private Equip sellingEquip;
+    //private Weapon sellingWeapon1;
+    private int priceSmallObject = 20;
+    private int priceBigObject = 50;
+    private int priceEquip;
+    //private int priceWeapon = 500;
+    private int quantitySmallObject = 5;
+    private int quantityBigObject = 2;
+    private int quantityEquip = 1;
+    //private int quantityWeapon;
+    private int typeObject1;
+    private int typeObject2;
 
-	public Merchant(int x, int y, int floor, Character mainCharacter) {
+    public Merchant(int x, int y, int floor, Character mainCharacter) {
 
-		this.name = "Rogelio";
-		this.x = x;
-		this.y = y;
-		this.floor = floor;
-		this.mainCharacter = mainCharacter;
-		this.sellingObject1 = generateSmallObject();
-		this.sellingObject2 = generateBigObject();
-		this.sellingEquip = equipGenerator();
+        this.name = "Rogelio";
+        this.x = x;
+        this.y = y;
+        this.floor = floor;
+        this.mainCharacter = mainCharacter;
+        this.sellingObject1 = generateSmallObject();
+        this.sellingObject2 = generateBigObject();
+        this.sellingEquip = equipGenerator();
 
-	}
+    }
 
-	private Equip equipGenerator() {
-		int random = rn.nextInt(4);
+    private Equip equipGenerator() {
+        int random = rn.nextInt(4);
 
-		Equip selection = new Equip();
+        Equip selection = new Equip();
 
-		switch (random) {
-			case 0:
-				selection = mainCharacter.newWeapons[floor];
-				priceEquip = 100 * floor;
-				break;
-			case 1:
-				selection = mainCharacter.newHelmets[floor];
-				priceEquip = 60 * floor;
-				break;
-			case 2:
-				selection = mainCharacter.newChestPlates[floor];
-				priceEquip = 80 * floor;
-				break;
-			case 3:
-				selection = mainCharacter.newLegArmors[floor];
-				priceEquip = 70 * floor;
-				break;
-		}
+        switch (random) {
+            case 0:
+                selection = mainCharacter.newWeapons[floor];
+                priceEquip = 100 * floor;
+                break;
+            case 1:
+                selection = mainCharacter.newHelmets[floor];
+                priceEquip = 60 * floor;
+                break;
+            case 2:
+                selection = mainCharacter.newChestPlates[floor];
+                priceEquip = 80 * floor;
+                break;
+            case 3:
+                selection = mainCharacter.newLegArmors[floor];
+                priceEquip = 70 * floor;
+                break;
+        }
 
-		return selection;
+        return selection;
 
-	}
+    }
 
-	private Object generateSmallObject() {
+    private Object generateSmallObject() {
 
-		Potion a;
+        Potion a;
 
-		switch (rn.nextInt(2)) {
+        switch (rn.nextInt(2)) {
 
-			case 0:
-				a = new Potion("Small Healing Potion", 30, "healing", "restores 30% health of your max HP", 2);
-				typeObject1 = 0;
-				break;
+            case 0:
+                a = new Potion("Small Healing Potion", 30, "healing", "restores 30% health of your max HP", 2);
+                typeObject1 = 0;
+                break;
 
-			case 1:
+            case 1:
 
-				a = new Potion("Small Mana Potion", 30, "mana", "restores 30% health of your max MP", 2);
-				typeObject1 = 2;
+                a = new Potion("Small Mana Potion", 30, "mana", "restores 30% health of your max MP", 2);
+                typeObject1 = 2;
 
-				break;
+                break;
 
-			default:
-				a = new Potion("Small Healing Potion", 30, "healing", "restores 30% health of your max HP", 2);
-				typeObject1 = 0;
+            default:
+                a = new Potion("Small Healing Potion", 30, "healing", "restores 30% health of your max HP", 2);
+                typeObject1 = 0;
 
-		}
+        }
 
-		return a;
+        return a;
 
-	}
+    }
 
-	private Object generateBigObject() {
+    private Object generateBigObject() {
 
-		Potion a;
+        Potion a;
 
-		switch (rn.nextInt(2)) {
+        switch (rn.nextInt(2)) {
 
-			case 0:
-				a = new Potion("Large Healing Potion", 50, "healing", "restores 50% health of your max HP", 2);
-				typeObject2 = 1;
+            case 0:
+                a = new Potion("Large Healing Potion", 50, "healing", "restores 50% health of your max HP", 2);
+                typeObject2 = 1;
 
-				break;
+                break;
 
-			case 1:
+            case 1:
 
-				a = new Potion("Large Mana Potion", 50, "mana", "restores 50% health of your max MP", 2);
-				typeObject2 = 3;
+                a = new Potion("Large Mana Potion", 50, "mana", "restores 50% health of your max MP", 2);
+                typeObject2 = 3;
 
-				break;
+                break;
 
-			default:
-				a = new Potion("Large Healing Potion", 50, "healing", "restores 50% health of your max HP", 2);
-				typeObject2 = 1;
+            default:
+                a = new Potion("Large Healing Potion", 50, "healing", "restores 50% health of your max HP", 2);
+                typeObject2 = 1;
 
-		}
+        }
 
-		return a;
+        return a;
 
-	}
+    }
 
-	public void trade() {
+    public void trade() {
 
-		boolean exit = false;
+        boolean exit = false,confirmation;
 
-		do {
+        do {
 
-			System.out.println("\nSHOP\n");
-			System.out.println("What do you want to buy?");
-			System.out.println("\n\t1-" + sellingObject1.getName() + "\n\t2-" + sellingObject2.getName() + "\n\t3-" + sellingEquip.getName() + "\n\t0- Exit the shop");
+            System.out.println("\nSHOP\n");
+            System.out.println("What do you want to buy?");
+            System.out.println("\n\t1-" + sellingObject1.getName() + " | " + priceSmallObject + " Gold" + "\n\t2-" + sellingObject2.getName() + " | " + priceBigObject + " Gold" + "\n\t3-" + sellingEquip.getName() + " | " + priceEquip + " Gold" + "\n\t0- Exit the shop");
+            System.out.println("\nCurrent money " + mainCharacter.getGold() + " Gold");
 
-			switch (Teclat.llegirInt()) {
+            switch (Teclat.llegirInt()) {
 
-				case 1:
+                case 1:
 
-					if (mainCharacter.getGold() < priceBigObject) {
-						System.out.println("You don't have enough money");
-					} else if (quantitySmallObject > 0) {
+                    if (mainCharacter.getGold() < priceSmallObject) {
 
-						System.out.println("You have bought a " + sellingObject1.toString());
+                        System.out.println("\nYou don't have enough money");
 
-						quantitySmallObject--;
+                    } else {
 
-						mainCharacter.setGold(mainCharacter.getGold() - priceSmallObject);
+                        confirmation = buyingConfirmation();
 
-						mainCharacter.getInventory()[typeObject1].setNumOfUses(mainCharacter.getInventory()[typeObject1].getNumOfUses() + 1);
+                        if (confirmation == true) {
 
-					}
+                            if (quantitySmallObject > 0) {
 
-					break;
+                                System.out.println("You have bought a " + sellingObject1.toString());
 
-				case 2:
+                                quantitySmallObject--;
 
-					if (mainCharacter.getGold() < priceBigObject) {
-						System.out.println("You don't have enough money");
-					} else if (quantityBigObject > 0) {
+                                mainCharacter.setGold(mainCharacter.getGold() - priceSmallObject);
 
-						System.out.println("You have bought a " + sellingObject2);
+                                mainCharacter.getInventory()[typeObject1].setNumOfUses(mainCharacter.getInventory()[typeObject1].getNumOfUses() + 1);
 
-						quantityBigObject--;
+                            }
+                        } else {
 
-						mainCharacter.setGold(mainCharacter.getGold() - priceBigObject);
+                            System.out.println("\nYou haven't bought anything");
 
-						mainCharacter.getInventory()[typeObject2].setNumOfUses(mainCharacter.getInventory()[typeObject2].getNumOfUses() + 1);
+                        }
 
-					}
+                    }
 
-					break;
+                    break;
 
-				case 3:
+                case 2:
 
-					if (quantityEquip > 0 && mainCharacter.getGold() >= priceEquip) {
+                    if (mainCharacter.getGold() < priceBigObject) {
 
-						System.out.println("You have bought " + sellingEquip.getName());
+                        System.out.println("\nYou don't have enough money");
 
-						quantityEquip--;
+                    } else {
 
-						mainCharacter.setGold(mainCharacter.getGold() - priceEquip);
+                        confirmation = buyingConfirmation();
 
-						mainCharacter.changeEquip(mainCharacter.getEquipment(), sellingEquip);
+                        if (confirmation == true) {
 
-					}
+                            if (quantityBigObject > 0) {
 
-					break;
+                                System.out.println("You have bought a " + sellingObject2);
 
-				case 0:
+                                quantityBigObject--;
 
-					exit = true;
+                                mainCharacter.setGold(mainCharacter.getGold() - priceBigObject);
 
-					System.out.println("See you <3");
+                                mainCharacter.getInventory()[typeObject2].setNumOfUses(mainCharacter.getInventory()[typeObject2].getNumOfUses() + 1);
 
-					break;
+                            }
+                        } else {
 
-				default:
-					System.out.println("I can't sell you anything if I don't have it");
+                            System.out.println("\nYou haven't bought anything");
 
-			}
+                        }
 
-		} while (!exit);
+                    }
 
-	}
+                    break;
 
-	public void setY(int y) {
-		this.y = y;
-	}
+                case 3:
 
-	public void setX(int x) {
-		this.x = x;
-	}
+                    if (mainCharacter.getGold() < priceEquip) {
+
+                        System.out.println("\nYou don't have enough money");
+
+                    } else {
+
+                        confirmation = buyingConfirmation();
+
+                        if (confirmation == true) {
+
+                            if (quantityEquip > 0) {
+
+                                System.out.println("You have bought " + sellingEquip.getName());
+
+                                quantityEquip--;
+
+                                mainCharacter.setGold(mainCharacter.getGold() - priceEquip);
+
+                                mainCharacter.changeEquip(mainCharacter.getEquipment(), sellingEquip);
+
+                            }
+
+                        } else {
+
+                            System.out.println("\nYou haven't bought anything");
+
+                        }
+
+                    }
+
+                    break;
+
+                case 0:
+
+                    exit = true;
+
+                    System.out.println("See you <3");
+
+                    break;
+
+                default:
+                    System.out.println("I can't sell you anything if I don't have it");
+
+            }
+
+        } while (!exit);
+
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    private boolean buyingConfirmation() {
+
+        String answer;
+        boolean correct = false, confirmation = false;
+
+        do {
+            System.out.println("Are you sure you want to buy this object? (Y/N)");
+            answer = Teclat.llegirString();
+
+            answer = answer.toUpperCase();
+
+            if (answer.equals("Y") || answer.equals("N")) {
+                
+                correct = true;
+                
+            }
+
+        } while (!correct);
+        
+        if (answer.equals("Y")) {
+            
+            confirmation = true;
+            
+        }
+
+        return confirmation;
+
+    }
 
 }
