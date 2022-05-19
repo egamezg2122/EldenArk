2fe907acd3e8a6a5bc82ce202ab51710837fe1b7
MarcelHomar <99321235+MarcelHomar@users.noreply.github.com>
2022-05-19 14:51:17 +0200

    database

---
+++ eldenark.sql
@@ -0,0 +1,12 @@
+DROP DATABASE EldenArk;
+CREATE DATABASE EldenArk;
+
+USE EldenArk;
+
+CREATE TABLE gamedata(
+	username varchar(20),
+    userlevel int,
+    image varchar(100))
+    ;
+    
+    
